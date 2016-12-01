1f1a18005397413754b6a13be6f4d1359635f984
Dima Beznos <beznos.d@gmail.com>
2016-12-01 23:01:15 +0200

    Now main func using event bubbling for listening events from inputs

--- src/form-progress.js
+++ src/form-progress.js
@@ -9,40 +9,36 @@
     let currentProgress = 0;
 
     // make all input types lowercase
-    inputTypes = inputTypes.map(function(type){
+    inputTypes = inputTypes.map((type) => {
       return type.toString().toLowerCase();
     });
 
-    // improve with bubbling, one callback to the form
-    [].forEach.call(form, (input) => {
-      if (inputTypes.indexOf(input.tagName.toLowerCase()) != -1) {
+    form.addEventListener('input', (evt) => {
+      let input = evt.target;
 
-        // increase progress
-        input.addEventListener('input', function() {
-          if (this.value.length !== 0 && !this.progressChecked) {
-            currentProgress += progressStep;
+      // increase progress
+      if (input.value.length !== 0 && !input.progressChecked) {
+        currentProgress += progressStep;
 
-            if (proggressAttr === 'style') {
-              progressEl.style.width = currentProgress + unit;              
-            }
+        if (proggressAttr === 'style') {
+          progressEl.style.width = currentProgress + unit;              
+        }
 
-            this.progressChecked = true;
-          }
+        input.progressChecked = true;
+      }
 
-          // decrease progress
-          if (this.value.length === 0 && this.progressChecked) {
-            currentProgress -= progressStep;
+      // decrease progress
+      if (input.value.length === 0 && input.progressChecked) {
+        currentProgress -= progressStep;
 
-            if (proggressAttr === 'style') {
-              progressEl.style.width = currentProgress + unit;              
-            }
+        if (proggressAttr === 'style') {
+          progressEl.style.width = currentProgress + unit;              
+        }
 
-            this.progressChecked = false;
-          }
-        });
-
+        input.progressChecked = false;
       }
     });
+
   }
 
   window.formProgress = formProgress;
